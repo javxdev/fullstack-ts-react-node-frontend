5141e635a81ed393b20bb0234bac1d650768fb15
javierspr <javxdev@gmail.com>
2024-09-18 17:08:24 -0300

    update mobile header interface

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,14 +4,14 @@ export default function Layout() {
   return (
     <>
         <header className="bg-slate-800">
-            <div className="px-4 md:p-10 mx-auto max-w-5xl py-10 lg:px-0">
-                <h1 className="text-xl text-center md:text-start md:text-3xl font-extrabold text-white uppercase">
+            <div className="px-4 md:p-10 mx-auto max-w-5xl py-10 md:px-0">
+                <h1 className="text-xl text-center lg:text-start md:text-2xl  lg:text-3xl font-extrabold text-white uppercase">
                     Products Administrator
                 </h1>
             </div>
         </header>
 
-        <main className="px-4 md:p-10 my-10 mx-auto max-w-5xl bg-white shadow-md">
+        <main className="px-1 md:p-10 my-10 mx-auto max-w-5xl bg-white md:shadow-md">
           <Outlet/>
         </main>
     </>
